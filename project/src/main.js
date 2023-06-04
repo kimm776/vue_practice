bea8485935ad26600197153ef142ef0cb2552270
kimm776 <kimm776@naver.com>
2023-06-04 18:06:13 +0900

    fortawesome import 수정

--- project/src/main.js
+++ project/src/main.js
@@ -7,9 +7,9 @@ import i18nPlugin from './plugins/i18n'
 import en from './i18n/en'
 import ko from './i18n/ko'
 import PageTitle from './components/fragments/PageTitle.vue'
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { faUserSecret, faUserClock } from '@fortawesome/free-solid-svg-icons'
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret, faUserClock } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 import VueLoading from 'vue-loading-overlay'
@@ -17,12 +17,11 @@ import 'vue-loading-overlay/dist/css/index.css'
 import VueGoodTablePlugin from 'vue-good-table-next'
 import 'vue-good-table-next/dist/vue-good-table-next.css'
 import VueApexCharts from 'vue3-apexcharts'
-
-// library.add(faUserSecret)
-// library.add(faUserClock)
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.js'
+
+library.add(faUserSecret)
+library.add(faUserClock)
 
 const i18nStrings = { en, ko }
 
@@ -44,7 +43,7 @@ app.use(store)
 app.use(router)
 app.mixin(mixin)
 app.component('page-title', PageTitle)
-// app.component('font-awesome-icon', FontAwesomeIcon)
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.use(i18nPlugin, i18nStrings)
 app.use(VueSweetalert2)
 app.use(VueLoading)
